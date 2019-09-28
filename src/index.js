0c251f84c2f3b6bc33bdc5dd0f06d7bf2438b0bf
droogadulce <ahigareda.mayra@gmail.com>
2019-09-28 12:23:38 -0500

    Corrección del segundo problema async/await y cuarto problema resuelto

--- src/index.js
+++ src/index.js
@@ -3,47 +3,50 @@ const $observe = document.getElementById('observe');
 const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
 //const API = 'https://rickandmortyapi.com/api/character/';
 
-window.onload = () => {
+window.onload = function() {
+  localStorage.removeItem('next_fetch');
   localStorage.clear();
 };
 
 const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      //debugger;
-      let nextURL = response.info.next;
-      localStorage.setItem('next_fetch', nextURL);
-      if (localStorage.getItem('next_fetch')) {
-        console.log(localStorage.getItem('next_fetch'));
-        const characters = response.results;
-        let output = characters
-          .map(character => {
-            return `
+  return fetch(api).then(response => response.json());
+};
+
+const renderData = response => {
+  let nextURL = response.info.next;
+  localStorage.setItem('next_fetch', nextURL);
+  if (localStorage.getItem('next_fetch')) {
+    console.log(localStorage.getItem('next_fetch'));
+    const characters = response.results;
+    let output = characters
+      .map(character => {
+        return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
     `;
-          })
-          .join('');
-        let newItem = document.createElement('section');
-        newItem.classList.add('Items');
-        newItem.innerHTML = output;
-        $app.appendChild(newItem);
-      } else {
-        loadData(API);
-      }
-    })
-    .catch(error => console.log(error));
+      })
+      .join('');
+    let newItem = document.createElement('section');
+    newItem.classList.add('Items');
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+  } else {
+    intersectionObserver.unobserve($observe);
+    let lastItem = document.createElement('section');
+    let output = '<p>Ya no hay personajes...</p>';
+    lastItem.innerHTML = output;
+    $app.appendChild(lastItem);
+  }
 };
 
 const loadData = async () => {
-  if (localStorage.getItem('next_fetch')) {
-    await getData(localStorage.getItem('next_fetch'));
-  } else {
-    await getData(API);
+  if (localStorage.getItem('next_fetch') === null) {
+    localStorage.setItem('next_fetch', API);
   }
+  const data = await getData(localStorage.getItem('next_fetch'));
+  renderData(data);
 };
 
 const intersectionObserver = new IntersectionObserver(
