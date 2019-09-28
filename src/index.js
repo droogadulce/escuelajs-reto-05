b28fc6a8ba0ab010b8950c7f481b440b85231561
droogadulce <ahigareda.mayra@gmail.com>
2019-09-28 10:52:59 -0500

    Primer problema resuelto

--- src/index.js
+++ src/index.js
@@ -6,33 +6,44 @@ const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      let nextURL = response.info.next;
+      localStorage.setItem('next_fetch', nextURL);
+      if (localStorage.getItem('next_fetch')) {
+        console.log(localStorage.getItem('next_fetch'));
+      }
+
       const characters = response.results;
-      let output = characters.map(character => {
-        return `
+      let output = characters
+        .map(character => {
+          return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
-    `
-      }).join('');
+    `;
+        })
+        .join('');
       let newItem = document.createElement('section');
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
-}
+};
 
 const loadData = () => {
   getData(API);
-}
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: '0px 0px 100% 0px'
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+);
 
 intersectionObserver.observe($observe);
